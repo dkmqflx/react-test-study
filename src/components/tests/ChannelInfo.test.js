7ed0f301fb41646ccc6ffba335e54972b6cc67e5
dkmqflx <dkmqflx@gmail.com>
2023-11-01 23:32:31 +0900

    7.6 채널 컴포넌트 테스트💡, 7.7 깔끔한 리팩토링 😎

--- src/components/tests/ChannelInfo.test.js
+++ src/components/tests/ChannelInfo.test.js
@@ -1,22 +1,52 @@
-import { screen, render } from "@testing-library/react";
+import { render, screen, waitFor } from "@testing-library/react";
 import { Route } from "react-router-dom";
 import { withAllContexts, withRouter } from "../../tests/utils";
 import ChannelInfo from "../ChannelInfo";
 
 describe("ChannelInfo", () => {
-  // 오브젝트 인스턴스를 흉내내는 가짜 오브젝트
   const fakeYoutube = {
     channelImageURL: jest.fn(),
   };
 
-  // 각각의 it이 실행된 다음 호출된다
-  // mock을 reset 해준다
   afterEach(() => fakeYoutube.channelImageURL.mockReset());
 
+  // 스냅샷 테스트로 변경해주었다.
   it("renders correctly", async () => {
     fakeYoutube.channelImageURL.mockImplementation(() => "url");
+    const { asFragment } = renderChannelInfo();
 
-    render(
+    // 바로 스냅샷 테스트 진행하면, 위의 url 받아오는 부분이 비동기이기 때문에
+    // url이 없어서 이름만 나온다
+    // url이 있을때 까지 기다려야 하기 때문에 render를 먼저 하고
+
+    // 이미지가 나타날 때 까지 기다렸다가
+    await screen.findByRole("img");
+
+    // 반환 받은 fragment로 스냅샷을 만든다
+    expect(asFragment()).toMatchSnapshot();
+  });
+
+  it("renders without URL", () => {
+    fakeYoutube.channelImageURL.mockImplementation(() => {
+      throw new Error("error");
+    });
+    renderChannelInfo();
+
+    expect(screen.queryByRole("img")).toBeNull();
+  });
+
+  // 정적 테스트인 스냅샷 테스트를 위해서 해주지만
+  // 명시적으로 나타내고 싶다면 테스트 추가해주어도 된다
+  it("renders with URL", async () => {
+    fakeYoutube.channelImageURL.mockImplementation(() => "url");
+
+    renderChannelInfo();
+
+    await screen.findByRole("img");
+  });
+
+  function renderChannelInfo() {
+    return render(
       withAllContexts(
         withRouter(
           <Route path="/" element={<ChannelInfo id="id" name="channel" />} />
@@ -24,15 +54,5 @@ describe("ChannelInfo", () => {
         fakeYoutube
       )
     );
-
-    // await waitFor(() => screen.getByText('channel'));
-    await screen.findByText("channel");
-  });
+  }
 });
-
-/**
- * withAllContexts의 내부에 있는 YoutubeApiContext.Provider를 사용하면
- * YoutubeClient 인스턴스를 만들게 되므로
- * 불가피하게 axios를 사용하게 되면서 나타는 에러
- * 그렇기 때문에 context에서 provider를 분리해준다
- */
