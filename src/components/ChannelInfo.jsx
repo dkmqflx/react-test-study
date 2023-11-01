d42d8ca4f7ef0fecaa15968b6507d6dd827edf29
dkmqflx <dkmqflx@gmail.com>
2023-11-01 22:46:08 +0900

    7.3 리액트 쿼리 테스트 하기

--- src/components/ChannelInfo.jsx
+++ src/components/ChannelInfo.jsx
@@ -1,18 +1,22 @@
-import React from 'react';
-import { useYoutubeApi } from '../context/YoutubeApiContext';
-import { useQuery } from '@tanstack/react-query';
+import React from "react";
+import { useYoutubeApi } from "../context/YoutubeApiContext";
+import { useQuery } from "@tanstack/react-query";
 
 export default function ChannelInfo({ id, name }) {
   const { youtube } = useYoutubeApi();
   const { data: url } = useQuery(
-    ['channel', id],
+    ["channel", id],
     () => youtube.channelImageURL(id),
     { staleTime: 1000 * 60 * 5 }
   );
+  // 리액트 쿼리 사용해서 외부에서 네트워크 통신해서 데이터 받아오고 있다
+  // 유닛 테스트 할 때 이런 네트워크 통신 하는 것 좋지 않다.
+  // 리액트 쿼리로 테스트 하는 방법으 공식문서의 testing 항목을 참조한다
+  // https://tanstack.com/query/v4/docs/react/guides/testing
   return (
-    <div className='flex my-4 mb-8 items-center'>
-      {url && <img className='w-10 h-10 rounded-full' src={url} alt={name} />}
-      <p className='text-lg font-medium ml-2'>{name}</p>
+    <div className="flex my-4 mb-8 items-center">
+      {url && <img className="w-10 h-10 rounded-full" src={url} alt={name} />}
+      <p className="text-lg font-medium ml-2">{name}</p>
     </div>
   );
 }
--- src/tests/utils.js
+++ src/tests/utils.js
@@ -1,4 +1,6 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { MemoryRouter, Routes } from "react-router-dom";
+import { YoutubeApiContext } from "../context/YoutubeApiContext";
 
 // 대부분의 컴포넌트가 라우트를 사용하기 때문에 아래처럼 만들어준다.
 export function withRouter(routes, initialEntry = "/") {
@@ -6,5 +8,40 @@ export function withRouter(routes, initialEntry = "/") {
     <MemoryRouter initialEntries={[initialEntry]}>
       <Routes>{routes}</Routes>
     </MemoryRouter>
+  );
+}
+
+// 아래 블로그 참고해서 설정해주면 테스팅하기 좋다
+// https://tkdodo.eu/blog/testing-react-query
+
+function createTestQueryClient() {
+  return new QueryClient({
+    defaultOptions: {
+      queries: {
+        // ✅ turns retries off
+        retry: false,
+      },
+      logger: {
+        log: console.log,
+        warn: console.warn,
+        error: () => {},
+      },
+    },
+  });
+}
+
+// App.js보면 youtubeApiContext.Provider 로 감싸져 있는 것을 알 수 있다
+// youtubeApiContext.Provider는 유투브를 사용할 수 있도록 인스턴스를 제공하는 역할
+// 테스트할 때 이런 유투브 구현 사항까지 의존하지 않도록 한다
+// 단위테스트 이기 때문에 컴포넌트 자체만 테스트 하도록, 나머지 의존 사항은 mocking 해준다
+
+// youtube를 사용할 수 있도록 인스턴스를 제공하는 Provider
+// 외부에서 가짜 mock 상태의 youtube를 전달받을 수 있도록 한다
+export function withAllContexts(children, youtube) {
+  const testClient = createTestQueryClient();
+  return (
+    <YoutubeApiContext.Provider value={youtube}>
+      <QueryClientProvider client={testClient}>{children}</QueryClientProvider>
+    </YoutubeApiContext.Provider>
   );
 }
